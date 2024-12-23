b0096be3cdfc45e7b45ae7b151cc73f06c739793
KartikAKGEC89 <s11kartik2111087@gmail.com>
2024-12-23 10:33:45 +0530

    Connect mongoose and cors

---
+++ univocbackend/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', true);
+// const messageroute = require('./Routes/message')
+const cors = require('cors')
+
+
+app.use(cors())
+// `node --trace-warnings ...`
+mongoose.connect("mongodb+srv://Registration:2111087@cluster0.wexksa1.mongodb.net/Univoc?retryWrites=true&w=majority")
+    .then(() => console.log('Connected....'))
+    .catch((err) => console.log(err));
+
+app.use(express.json());
+  
+// app.use('/cloud', messageroute)
+
+app.listen(3000, () => console.log("Running"));
